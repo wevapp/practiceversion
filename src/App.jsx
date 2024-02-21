1dc535f2d5fac4119f3380020685216036b4b12e
wevapp <raymondestrella1993@gmail.com>
2024-02-21 14:07:21 +0800

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Button from './components/Button'
 const App = () => {
   return (
-    <div className='flex justify-center items-center h-screen font-bold bg-violet-500 text-white'>App</div>
+    <div className='flex justify-center items-center h-screen font-bold bg-violet-500 text-white'>
+      App <br />
+      <Button />
+    </div>
   )
 }
 
