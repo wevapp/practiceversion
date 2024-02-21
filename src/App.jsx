1b26a0629b29fb548b893865fb1977a6b9d0851a
wevapp <raymondestrella1993@gmail.com>
2024-02-21 13:33:56 +0800

    bg rose

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='flex justify-center items-center h-screen font-bold bg-blue-500'>App</div>
+    <div className='flex justify-center items-center h-screen font-bold bg-rose-500'>App</div>
   )
 }
 
