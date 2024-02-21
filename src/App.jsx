a91c79f79a9ef74d95a6a7fb41e94a4d42f754e2
wevapp <raymondestrella1993@gmail.com>
2024-02-21 14:10:08 +0800

    bg orange

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from './components/Button'
 const App = () => {
   return (
-    <div className='flex justify-center items-center h-screen font-bold bg-violet-500 text-white'>
+    <div className='flex justify-center items-center h-screen font-bold bg-orange-500 text-white'>
       App <br />
       <Button />
     </div>
