6ddcb345de791d351d6e507f67a6f693c85563e9
wevapp <raymondestrella1993@gmail.com>
2024-02-21 13:00:04 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen font-bold bg-teal-500'>App</div>
+  )
+}
+
+export default App
